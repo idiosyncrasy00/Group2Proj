a27540cbacdb90ac66cf73b033274af5c86b670d
Duc Hoang <davishoang20@gmail.com>
2021-10-11 00:37:43 +0700

    [FRONTEND] add and delete a meeting room test
    test the view feature

--- client/src/views/Rooms/RoomDetails.vue
+++ client/src/views/Rooms/RoomDetails.vue
@@ -1,15 +1,20 @@
 <template>
-  <h1>Room details page</h1>
+  <div>
+    <h1>Room details page</h1>
+    <p>Room id is {{ id }}</p>
+  </div>
 </template>
 
 <script>
 //import { defineComponent } from '@vue/composition-api'
-
 export default {
-  
-}
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
