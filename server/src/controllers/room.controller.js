09eb0d019cb849c0e92ec20910f2bee959c3e150
Duc Hoang <davishoang20@gmail.com>
2021-11-07 21:58:29 +0700

    Update room.controller.js

--- server/src/controllers/room.controller.js
+++ server/src/controllers/room.controller.js
@@ -67,19 +67,19 @@ const getRoomList = async (req, res) => {
 //         }
 //     }
 // };
-const createRoom = async (req, res) => {
-    if (!req.user.isAdmin) {
-        res.status(401).send({ error: "User has no right to create room" });
-    } else {
-        try {
-            let room = await Room.create(req.body);
-            res.send(_.pick(room, ['roomName', 'status']));
-            console.log('Room created');
-        } catch(error) {
-            res.status(400).send({ error: error.message });
-        }
-    }
-};
+// const createRoom = async (req, res) => {
+//     if (!req.user.isAdmin) {
+//         res.status(401).send({ error: "User has no right to create room" });
+//     } else {
+//         try {
+//             let room = await Room.create(req.body);
+//             res.send(_.pick(room, ['roomName', 'status']));
+//             console.log('Room created');
+//         } catch(error) {
+//             res.status(400).send({ error: error.message });
+//         }
+//     }
+// };
 
 module.exports = { getRoomList, createRoom };
 module.exports = { createRoom, getRoomList, editRoom };
