c3730c9777c1e6df517e07ef823c3a161be15f70
XanMarta <gmxancrazy24114@gmail.com>
2021-11-09 09:12:49 +0700

    Add deleteRoom api. Complete room APIs

--- server/src/routes/room.route.js
+++ server/src/routes/room.route.js
@@ -5,5 +5,6 @@ const auth = require('../middlewares/auth');
 router.post('/create', auth, Room.createRoom);
 router.get('/list', auth, Room.getRoomList);
 router.put('/edit', auth, Room.editRoom);
+router.delete('/delete', auth, Room.deleteRoom);
 
 module.exports = router;
