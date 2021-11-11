55f3f3d5dbe4035c3d28249dd0022b258875534f
Duc Hoang <davishoang20@gmail.com>
2021-11-11 23:55:42 +0700

    display room

--- client/src/views/Rooms/FindRoom.vue
+++ client/src/views/Rooms/FindRoom.vue
@@ -2,24 +2,29 @@
   <div>
     <div id="search-sec mx-auto">
       <div class="container">
-        <form action="#" method="post" novalidate="novalidate" @submit.enter.prevent=""	>
-					<div class = "row mb-5 mt-5">
-						<div class="col-sm-6 mx-auto">
-							<h3 class = "text-center"> Find your conference room </h3>
-						</div>
-					</div>
+        <form
+          action="#"
+          method="post"
+          novalidate="novalidate"
+          @submit.enter.prevent=""
+        >
+          <div class="row mb-5 mt-5">
+            <div class="col-sm-6 mx-auto">
+              <h3 class="text-center">Find your conference room</h3>
+            </div>
+          </div>
 
-					<div class="row">
-						<div class="col-sm-6 mx-auto">
-							<p style="color:gray; font-size:80%;">Please enter meeting ID</p>
-							<input
-							type="text"
-							class="form-control search-slt border-2 bg-light"
-							placeholder="Meeting's ID"
-							v-model="searchValues.roomid"
-							/>
-						</div>
-						<!--div class="col-lg-3 col-md-3 col-sm-12 p-0">
+          <div class="row">
+            <div class="col-sm-6 mx-auto">
+              <p style="color: gray; font-size: 80%">Please enter meeting ID</p>
+              <input
+                type="text"
+                class="form-control search-slt border-2 bg-light"
+                placeholder="Meeting's ID"
+                v-model="searchValues.roomid"
+              />
+            </div>
+            <!--div class="col-lg-3 col-md-3 col-sm-12 p-0">
 										<input
 							type="text"
 							class="form-control search-slt border-2 bg-light"
@@ -27,10 +32,10 @@
 							v-model="searchValues.roomname"
 										/>
 						</div-->
-					</div>
-					<div class="row mt-5">
-						<div class="col-sm-2 mx-auto">
-							<!--router-link :to="{ 
+          </div>
+          <div class="row mt-5">
+            <div class="col-sm-2 mx-auto">
+              <!--router-link :to="{ 
 								name: 'roomdetails', 
 								params: { id: 123456789 } 
 							}">
@@ -38,43 +43,51 @@
 									{{ room.id }}
 								</h5>
 							</router-link-->
-							<button 
-							type="button" 
-							class="btn btn-primary"
-							v-on:click="isHidden = false">
-								Search
-							</button>
-						</div>
-					</div>
-				</form>
+              <button
+                type="button"
+                class="btn btn-primary"
+                v-on:click="isHidden = false"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
     <section id="search-res" v-if="!isHidden">
       <div class="row">
-        <div
-          class="col-sm-6"
-          v-for="room in searchRooms.slice(0, 10)"
-          :key="room.ID"
-        >
-					<div class="card mb-2 mt-1" style="width:85%; height:95%">
-						<div style="padding-left:1rem;">
-							<router-link :to="{ name: 'roomdetails', params: { id: room.id } }">
-											<h5 class="card-title text-center text-dark">
-												{{ room.id }}
-											</h5>
-							</router-link>
-							<div class="row">
-								<div class="card-text col-sm-7">
-									<h6>Date: {{ room.reservedDate }}      </h6>
-									<h6> Start time: {{ room.startingTime }}	  </h6>
-								</div>
-								<div class="card-text col-sm-5">
-									<h6>Status: {{ room.status }}</h6>
-									<h6>Duration: {{ room.during }}</h6>
-								</div>
-							</div>
-						</div>
-					</div>	
+        <div class="col-sm-6" v-for="room in rooms" :key="room.id">
+          <div class="card mb-2 mt-1" style="width: 85%; height: 95%">
+            <div style="padding-left: 1rem">
+              <router-link
+                :to="{
+                  name: 'roomdetails',
+                  params: {
+                    id: room.id,
+                    roomname: room.roomname,
+                    capacity: room.capacity,
+                    facilities: room.facilities,
+                    status: room.status,
+                  },
+                }"
+              >
+                <h5 class="card-title text-center text-dark">
+                  {{ room.id }}
+                </h5>
+              </router-link>
+              <div class="row">
+                <div class="card-text col-sm-7">
+                  <h6>Date: {{ room.roomname }}</h6>
+                  <h6>Start time: {{ room.capacity }}</h6>
+                </div>
+                <div class="card-text col-sm-5">
+                  <h6>Status: {{ room.facilities }}</h6>
+                  <h6>Duration: {{ room.status }}</h6>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -83,43 +96,71 @@
 
 <script>
 import Nav from "@/components/Nav.vue";
-import DataServices from "@/services/DataServices.js";
+//import DataServices from "@/services/DataServices.js";
+import axios from "axios";
 
 export default {
   name: "FindRoom",
   data() {
     return {
-			isHidden: true,
+      isHidden: true,
       searchValues: {
-		roomname: "",
+        roomname: "",
         city: "",
         district: "",
         roomid: "",
         status: "",
       },
+      //roomlist: "",
       rooms: [
-        { id: 123456789, reservedDate: "01/01/1999", startingTime: "15:45 CH", during: "5", status: "active" },
-				{ id: 123456789, reservedDate: "01/01/1999", startingTime: "15:45 CH", during: "5", status: "active" },
-        { id: 453452123, reservedDate: "01/01/2000", startingTime: "15:45 CH", during: "5", status: "active" },
-				{ id: 787878123, reservedDate: "01/01/2001", startingTime: "15:45 CH", during: "5", status: "comming" },
-				{ id: 123123123, reservedDate: "01/01/2002", startingTime: "15:45 CH", during: "24", status: "active" },
-				{ id: 78452512, reservedDate: "01/01/2003", startingTime: "15:45 CH", during: "5", status: "ended" },
-        
+        // {
+        //   id: this.roomlist.id,
+        //   roomname: this.roomlist.roomname,
+        //   capacity: this.roomlist.capacity,
+        //   facilities: this.roomlist.facilities,
+        //   status: this.roomlist.status,
+        // },
+        // {
+        //   id: 123456789,
+        //   roomname: "",
+        //   capacity: "",
+        //   facilities: "",
+        //   status: "active",
+        // },
+        // {
+        //   id: 453452123,
+        //   roomname: "",
+        //   capacity: "",
+        //   facilities: "",
+        //   status: "active",
+        // },
+        // {
+        //   id: 787878123,
+        //   roomname: "",
+        //   capacity: "",
+        //   facilities: "",
+        //   status: "active",
+        // },
+        // {
+        //   id: 123123123,
+        //   roomname: "",
+        //   capacity: "",
+        //   facilities: "",
+        //   status: "active",
+        // },
+        // {
+        //   id: 78452512,
+        //   roomname: "",
+        //   capacity: "",
+        //   facilities: "",
+        //   status: "active",
+        // },
       ],
     };
   },
-  created() {
-    DataServices.getRoom()
-      .then((res) => {
-        this.event = res.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
   components: {
     Nav,
-    DataServices,
+    //DataServices,
   },
   method: {
     searchResult() {
@@ -128,16 +169,30 @@ export default {
   },
   computed: {
     searchRooms() {
-			if (this.searchValues.roomid.length < 5)
-				return this.rooms.filter(
-        (rooms) =>
-			rooms.id.toString().startsWith("#")
-      );
-      return this.rooms.filter(
-        (rooms) =>
-			rooms.id.toString().startsWith(this.searchValues.roomid)
+      if (this.searchValues.rooms.length < 5)
+        return this.rooms.filter((rooms) =>
+          rooms.id.toString().startsWith(this.searchValues.roomid)
+        );
+      return this.rooms.filter((rooms) =>
+        rooms.id.toString().startsWith(this.searchValues.roomid)
       );
     },
+  },
+  async created() {
+    axios
+      .get("/api/rooms/list", {
+        headers: {
+          accesstoken: localStorage.getItem("accesstoken"),
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        this.rooms = res.data;
+        //this.userInfo = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
@@ -149,28 +204,27 @@ export default {
 #search-res {
   padding: 50px 100px 100px 200px;
 }
-.tab { margin-right: 5px; }
+.tab {
+  margin-right: 5px;
+}
 .card {
-	margin-top:5%;
-  -webkit-transition: all .2s ease-out;
-	-moz-transition: all .2s ease-out;
-	-ms-transition: all .2s ease-out;
-	-o-transition: all .2s ease-out;
-	transition: all .2s ease-out;
-	border-radius:15px;
-	border:none;
-	background-color:var(--bg-color);
+  margin-top: 5%;
+  -webkit-transition: all 0.2s ease-out;
+  -moz-transition: all 0.2s ease-out;
+  -ms-transition: all 0.2s ease-out;
+  -o-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+  border-radius: 15px;
+  border: none;
+  background-color: var(--bg-color);
 }
-.card:hover .btn
-{
-opacity:1;
-top: 100%;
- box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
+.card:hover .btn {
+  opacity: 1;
+  top: 100%;
+  box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
 }
 
-.card:hover
-{
-box-shadow: 0 4px 18px 0 rgba(0,0,0,0.7);
-
+.card:hover {
+  box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.7);
 }
 </style>
--- client/src/views/Rooms/RoomDetails.vue
+++ client/src/views/Rooms/RoomDetails.vue
@@ -1,29 +1,34 @@
 <template>
   <div>
     <!--h1>Room details page</h1-->
-		<div class="card col-sm-5 mx-auto mt-5 border-top border-5 rounded" 
-		style="background-color:var(--bg-color);">
-			<h3 class="mt-1 mx-auto "> {{info.meetingID}} </h3>
-			<div class="row mt-2">
-					<div class="col-sm-5 mx-auto">
-						<p class="info-text">Host name:</p>
-						<p class="info-text">Start time: {{info.startingTime}}</p>
-						<p class="info-text">Duration : {{info.during}}</p>
-					</div>
-					<div class="col-sm-5 mx-auto">
-						<p class="info-text">Group: </p>
-						<p class="info-text">Date : {{info.reseverDate}}</p>
-						<p class="info-text">Status: {{info.status}}</p>
-					</div>
-			</div>
-			<div class="row mt-2 mb-2">
-				<button class="btn btn-primary col-sm-3 mx-auto" 
-				@click="joinMeeting"
-				> 
-					Join now ! 
-				</button>
-			</div>
-		</div>
+    <div
+      class="card col-sm-5 mx-auto mt-5 border-top border-5 rounded"
+      style="background-color: var(--bg-color)"
+    >
+      {{ this.id }}
+      {{ this.roomname }}
+      {{ this.capacity }}
+      {{ this.facilities }}
+      {{ this.status }}
+      <h3 class="mt-1 mx-auto">{{ info.meetingID }}</h3>
+      <div class="row mt-2">
+        <div class="col-sm-5 mx-auto">
+          <p class="info-text">Host name:</p>
+          <p class="info-text">Start time: {{ info.startingTime }}</p>
+          <p class="info-text">Duration : {{ info.during }}</p>
+        </div>
+        <div class="col-sm-5 mx-auto">
+          <p class="info-text">Group:</p>
+          <p class="info-text">Date : {{ info.reseverDate }}</p>
+          <p class="info-text">Status: {{ info.status }}</p>
+        </div>
+      </div>
+      <div class="row mt-2 mb-2">
+        <button class="btn btn-primary col-sm-3 mx-auto" @click="joinMeeting">
+          Join now !
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,24 +39,29 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
-			info: { meetingID: 123456789,
-			adminID: 2345,
-			startingTime: "23:45 CH",
-			reseverDate: "01/01/1234",
-			during:3,
-			status: "active",
-			},
+      roomname: this.$route.params.roomname,
+      capacity: this.$route.params.capacity,
+      facilities: this.$route.params.facilities,
+      status: this.$route.params.status,
+      info: {
+        meetingID: 123456789,
+        adminID: 2345,
+        startingTime: "23:45 CH",
+        reseverDate: "01/01/1234",
+        during: 3,
+        status: "active",
+      },
     };
   },
-	method:{
-		joinMeeting() {
-			//TODO
+  method: {
+    joinMeeting() {
+      //TODO
     },
-	},
+  },
   components: {
     DataServices,
   },
-	/* 
+  /* 
   created() {
     DataServices.getRoomInfo(this.id)
     .then(res => {
@@ -65,9 +75,9 @@ export default {
 </script>
 
 <style>
-.info-text{
-	font-size:1.15em;
-	font-family: "Lucida Console", "Courier New", monospace;
-	font-weight: bold;
+.info-text {
+  font-size: 1.15em;
+  font-family: "Lucida Console", "Courier New", monospace;
+  font-weight: bold;
 }
 </style>
