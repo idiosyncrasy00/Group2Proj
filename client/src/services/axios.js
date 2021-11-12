40530fa57dfd76514765e23aca853a3e186ca2d2
XanMarta <gmxancrazy24114@gmail.com>
2021-11-12 11:24:53 +0700

    Add environment variables

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3000/';
+axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
 axios.defaults.headers.common['Authorization'];
