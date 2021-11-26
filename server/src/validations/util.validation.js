961c26f5f6a970095736cc9c4198022fec6b8ab9
XanMarta <gmxancrazy24114@gmail.com>
2021-11-27 01:01:06 +0700

    Fix query, add invite, delete meeting APIs

--- server/src/validations/util.validation.js
+++ server/src/validations/util.validation.js
@@ -2,6 +2,7 @@ const Joi = require('joi').extend(require('@joi/date'));
 
 
 const querySchema = Joi.object({
+    adminid: Joi.number().integer(),
     adminname: Joi.string().empty(null).default(""),
     roomname: Joi.string().empty(null).default(""),
     reserveddate: Joi.date().format('YYYY-MM-DD').empty(null).default(""),
